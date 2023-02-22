fcf0b46abf42a0ff798fd542c951d033eb58dbac
Fainsteve <fainstevekassums@gmail.com>
2023-02-22 00:41:47 -0800

    22

--- _atoi.c
+++ _atoi.c
@@ -5,8 +5,7 @@
 * @info: struct address
 *
 * Return: 1 if interactive mode, 0 otherwise
-*/
-int interactive(info_t *info)
+*/int interactive(info_t *info)
 {
 return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
@@ -16,8 +15,7 @@ return (isatty(STDIN_FILENO) && info->readfd <= 2);
 * @c: the char to check
 * @delim: the delimiter string
 * Return: 1 if true, 0 if false
-*/
-int is_delim(char c, char *delim)
+*/int is_delim(char c, char *delim)
 {
 while (*delim)
 {
@@ -31,8 +29,7 @@ return (0);
 * _isalpha - checks for alphabetic character
 * @c: The character to input
 * Return: 1 if c is alphabetic, 0 otherwise
-*/
-int _isalpha(int c)
+*/int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 return (1);
@@ -44,8 +41,7 @@ return (0);
 * _atoi - converts a string to an integer
 * @s: the string to be converted
 * Return: 0 if no numbers in string, converted number otherwise
-*/
-int _atoi(char *s)
+*/int _atoi(char *s)
 {
 int i, sign = 1, flag = 0, output = 0;
 unsigned int result = 0;
